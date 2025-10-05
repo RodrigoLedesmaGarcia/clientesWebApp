8c458058948fb5478a0de27aab9af41ae6649959
RodrigoLedesmaGarcia <rodrigo_ledesma_garcia@outlook.com>
2025-10-05 02:29:01 -0600

    docker compose 3

--- ms-clientes/src/test/java/com/spring/www/ms_clientes/MsClientesApplicationTests.java
+++ ms-clientes/src/test/java/com/spring/www/ms_clientes/MsClientesApplicationTests.java
@@ -1,13 +1,32 @@
 package com.spring.www.ms_clientes;
 
+import net.bytebuddy.utility.dispatcher.JavaDispatcher;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest(
-        properties = "spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration"
-)
+import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@SpringBootTest
+@Testcontainers
+@EnabledIfEnvironmentVariable(named = "USE_TESTCONTAINERS", matches = "true")
 class MsClientesApplicationTests {
-    @Test
-    void contextLoads() {}
+
+    static MySQLContainer<?> mysql = new MySQLContainer<>("mysql:8.4")
+            .withDatabaseName("clientes_test")
+            .withUsername("testuser")
+            .withPassword("testpass");
+
+    @DynamicPropertySource
+    static void dbProps(DynamicPropertyRegistry r) {
+        r.add("spring.datasource.url", mysql::getJdbcUrl);
+        r.add("spring.datasource.username", mysql::getUsername);
+        r.add("spring.datasource.password", mysql::getPassword);
+        r.add("spring.jpa.hibernate.ddl-auto", () -> "update");
+    }
+
+    @Test void contextLoads() {}
 }
